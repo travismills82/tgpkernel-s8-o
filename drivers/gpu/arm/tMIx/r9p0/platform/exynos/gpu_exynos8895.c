1cd435fc7bbf3ecd25134f9d4ec934443837f84e
Travis Mills <travismills82@gmail.com>
2019-01-06 19:34:20 -0800

    mali: optimize gpu speed

--- drivers/gpu/arm/tMIx/r9p0/platform/exynos/gpu_exynos8895.c
+++ drivers/gpu/arm/tMIx/r9p0/platform/exynos/gpu_exynos8895.c
@@ -79,9 +79,9 @@ static gpu_dvfs_info gpu_dvfs_table_default[] = {
 	{839, 850000, 0, 98, 100, 1, 0, 1794000, 400000, 1690000, CPU_MAX},
 	{764, 850000, 0, 98, 100, 1, 0, 1794000, 400000, 1690000, CPU_MAX},
 	{683, 800000, 0, 98, 100, 1, 0, 1794000, 400000, 1690000, CPU_MAX},
-	{572, 800000, 0, 78,  99, 5, 0, 1794000, 400000, 1690000, CPU_MAX},
-	{546, 800000, 0, 78,  99, 5, 0, 1794000, 400000, 1690000, 2002000},
-	{455, 800000, 0, 78,  85, 9, 0, 1540000, 400000, 1456000, 2002000},
+	{572, 800000, 0, 78,  99, 1, 0, 1794000, 400000, 1690000, CPU_MAX},
+	{546, 800000, 0, 78,  99, 1, 0, 1794000, 400000, 1690000, CPU_MAX},
+	{455, 800000, 0, 78,  85, 1, 0, 1540000, 400000, 1456000, CPU_MAX},
 	{385, 800000, 0, 78,  85, 1, 0, 1352000, 400000, 1248000, CPU_MAX},
 	{338, 800000, 0, 78,  85, 1, 0, 1014000, 267000,  949000, CPU_MAX},
 	{260, 800000, 0, 78,  85, 1, 0,  421000, 178000,       0, CPU_MAX},
@@ -96,12 +96,12 @@ static int mif_min_table[] = {
 };
 
 static gpu_attribute gpu_config_attributes[] = {
-	{GPU_MAX_CLOCK, 546},
-	{GPU_MAX_CLOCK_LIMIT, 546},
+	{GPU_MAX_CLOCK, 572},
+	{GPU_MAX_CLOCK_LIMIT, 572},
 	{GPU_MIN_CLOCK, 260},
 	{GPU_DVFS_START_CLOCK, 260},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 260},
-	{GPU_GOVERNOR_TYPE, G3D_DVFS_GOVERNOR_INTERACTIVE},
+	{GPU_GOVERNOR_TYPE, G3D_DVFS_GOVERNOR_DEFAULT},
 	{GPU_GOVERNOR_START_CLOCK_DEFAULT, 260},
 	{GPU_GOVERNOR_START_CLOCK_INTERACTIVE, 260},
 	{GPU_GOVERNOR_START_CLOCK_STATIC, 260},
@@ -114,7 +114,7 @@ static gpu_attribute gpu_config_attributes[] = {
 	{GPU_GOVERNOR_TABLE_SIZE_INTERACTIVE, GPU_DVFS_TABLE_LIST_SIZE(gpu_dvfs_table_default)},
 	{GPU_GOVERNOR_TABLE_SIZE_STATIC, GPU_DVFS_TABLE_LIST_SIZE(gpu_dvfs_table_default)},
 	{GPU_GOVERNOR_TABLE_SIZE_BOOSTER, GPU_DVFS_TABLE_LIST_SIZE(gpu_dvfs_table_default)},
-	{GPU_GOVERNOR_INTERACTIVE_HIGHSPEED_CLOCK, 455},
+	{GPU_GOVERNOR_INTERACTIVE_HIGHSPEED_CLOCK, 0},
 	{GPU_GOVERNOR_INTERACTIVE_HIGHSPEED_LOAD, 95},
 	{GPU_GOVERNOR_INTERACTIVE_HIGHSPEED_DELAY, 0},
 	{GPU_DEFAULT_VOLTAGE, 800000},
@@ -140,14 +140,14 @@ static gpu_attribute gpu_config_attributes[] = {
 	{GPU_DVFS_POLLING_TIME, 30},
 	{GPU_PMQOS_INT_DISABLE, 1},
 	{GPU_PMQOS_MIF_MAX_CLOCK, 1794000},
-	{GPU_PMQOS_MIF_MAX_CLOCK_BASE, 546},
-	{GPU_CL_DVFS_START_BASE, 455},
+	{GPU_PMQOS_MIF_MAX_CLOCK_BASE, 572},
+	{GPU_CL_DVFS_START_BASE, 572},
 	{GPU_DEBUG_LEVEL, DVFS_WARNING},
 	{GPU_TRACE_LEVEL, TRACE_ALL},
-	{GPU_MO_MIN_CLOCK, 455},
+	{GPU_MO_MIN_CLOCK, 572},
 	{GPU_BOOST_EGL_MIN_LOCK, 1872000},
 #ifdef CONFIG_MALI_VK_BOOST
-	{GPU_VK_BOOST_MAX_LOCK, 338},
+	{GPU_VK_BOOST_MAX_LOCK, 572},
 	{GPU_VK_BOOST_MIF_MIN_LOCK, 1794000},
 #endif
 	{GPU_CONFIG_LIST_END, 0}
@@ -622,8 +622,8 @@ int gpu_asv_calibration_start(void)
 	}
 
 	gpu_control_power_policy_set(pkbdev, "always_on");
-	gpu_dvfs_clock_lock(GPU_DVFS_MAX_LOCK, ASV_CALI_LOCK, 546);
-	gpu_dvfs_clock_lock(GPU_DVFS_MIN_LOCK, ASV_CALI_LOCK, 546);
+	gpu_dvfs_clock_lock(GPU_DVFS_MAX_LOCK, ASV_CALI_LOCK, 572);
+	gpu_dvfs_clock_lock(GPU_DVFS_MIN_LOCK, ASV_CALI_LOCK, 572);
 	gpu_pm_qos_command(platform, GPU_CONTROL_PM_QOS_RESET);
 	return 0;
 }
@@ -662,8 +662,8 @@ int gpu_asv_calibration_start_wq(void)
 
 	platform->gpu_auto_cali_status = true;
 	gpu_control_power_policy_set(pkbdev, "always_on");
-	gpu_dvfs_clock_lock(GPU_DVFS_MAX_LOCK, ASV_CALI_LOCK, 546);
-	gpu_dvfs_clock_lock(GPU_DVFS_MIN_LOCK, ASV_CALI_LOCK, 546);
+	gpu_dvfs_clock_lock(GPU_DVFS_MAX_LOCK, ASV_CALI_LOCK, 572);
+	gpu_dvfs_clock_lock(GPU_DVFS_MIN_LOCK, ASV_CALI_LOCK, 572);
 
 	if (NULL == gpu_asv_cali_wq) {
 		INIT_DELAYED_WORK(&gpu_asv_cali_stop_work, gpu_asv_calibration_stop_callback);
